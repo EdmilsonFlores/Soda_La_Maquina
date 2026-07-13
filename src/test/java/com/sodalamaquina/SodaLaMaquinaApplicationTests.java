e7c51dd86f517d61aa1edb91ed9db83f32f1c271
DanielbigM <danielmurillocr@hotmail.com>
2026-07-12 18:00:07 -0600

    Proyecto base Spring Boot inicial

---
+++ src/test/java/com/sodalamaquina/SodaLaMaquinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sodalamaquina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SodaLaMaquinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
