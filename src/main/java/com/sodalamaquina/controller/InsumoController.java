f6a60ff52ad9a27e4d27c15f435c75ca807dd328
DanielbigM <danielmurillocr@hotmail.com>
2026-07-14 20:35:02 -0600

    Formulario básico para registrar insumos

--- src/main/java/com/sodalamaquina/controller/InsumoController.java
+++ src/main/java/com/sodalamaquina/controller/InsumoController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequestMapping("/insumo")
@@ -32,4 +33,12 @@ public class InsumoController {
 
         return "/insumo/listado";
     }
+    
+    @PostMapping("/guardar")
+    public String guardar(Insumo insumo) {
+
+        insumoService.save(insumo);
+
+    return "redirect:/insumo/listado";
+}
 }
