4118a0d09cad7ac130ad339b6f3ce57b354b59a6
DanielbigM <danielmurillocr@hotmail.com>
2026-07-14 19:19:52 -0600

    Listado inicial de insumos funcionando

---
+++ src/main/java/com/sodalamaquina/controller/InsumoController.java
@@ -0,0 +1,35 @@
+package com.sodalamaquina.controller;
+
+import com.sodalamaquina.domain.Insumo;
+import com.sodalamaquina.service.InsumoService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/insumo")
+public class InsumoController {
+
+    private final InsumoService insumoService;
+
+    public InsumoController(InsumoService insumoService) {
+        this.insumoService = insumoService;
+    }
+
+    @GetMapping("/listado")
+    public String listado(Model model) {
+
+        model.addAttribute(
+                "insumos",
+                insumoService.getInsumos(false)
+        );
+
+        model.addAttribute(
+                "insumo",
+                new Insumo()
+        );
+
+        return "/insumo/listado";
+    }
+}
