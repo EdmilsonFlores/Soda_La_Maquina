a51b64042b16811a17062abe2925b24c23fab4be
DanielbigM <danielmurillocr@hotmail.com>
2026-08-18 21:59:04 -0600

    Actualización de Interfaz y migración de MySQL a TiDB Cloud
    Se actualizo la interfaz de el:
    -Dashboard
    -Gestion de Insumos
    -Categorías
    
    Se migraron los datos de MySQL a una nube (TiDB Cloud)

--- src/main/java/com/sodalamaquina/controller/HomeController.java
+++ src/main/java/com/sodalamaquina/controller/HomeController.java
@@ -1,13 +1,45 @@
 package com.sodalamaquina.controller;
 
+import com.sodalamaquina.service.CategoriaInsumoService;
+import com.sodalamaquina.service.InsumoService;
+import com.sodalamaquina.service.ProveedorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private InsumoService insumoService;
+
+    @Autowired
+    private CategoriaInsumoService categoriaInsumoService;
+
+    @Autowired
+    private ProveedorService proveedorService;
+
     @GetMapping("/")
-    public String inicio() {
+    public String index(Model model) {
+        var listaInsumos = insumoService.getInsumos();
+        var listaCategorias = categoriaInsumoService.getCategorias(true);
+        var listaProveedores = proveedorService.getProveedores();
+
+        // Filtrar insumos donde la cantidad actual es menor o igual al mínimo
+        var listaStockBajo = listaInsumos.stream()
+                .filter(i -> i.getCantidadActual() != null
+                && i.getAlertaMinima() != null
+                && i.getCantidadActual() <= i.getAlertaMinima())
+                .toList();
+
+        // Pasar totales y lista de alertas a la vista
+        model.addAttribute("totalInsumos", listaInsumos.size());
+        model.addAttribute("totalCategorias", listaCategorias.size());
+        model.addAttribute("totalProveedores", listaProveedores.size());
+        model.addAttribute("totalStockBajo", listaStockBajo.size());
+        model.addAttribute("insumosStockBajo", listaStockBajo);
+
         return "index/index";
     }
 }
